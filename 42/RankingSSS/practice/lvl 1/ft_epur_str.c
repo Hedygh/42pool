76aee6fc1b01b4ac232f5db0edb4b2f2a43e29b7
Hedy OUAHES <heouahes@e3r10p20.42.fr>
2020-08-27 11:40:28 +0200

    daa

--- 42/RankingSSS/practice/lvl 1/ft_epur_str.c
+++ 42/RankingSSS/practice/lvl 1/ft_epur_str.c
@@ -6,39 +6,42 @@
 /*   By: heouahes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/26 20:36:21 by heouahes          #+#    #+#             */
-/*   Updated: 2020/08/26 21:10:40 by heouahes         ###   ########.fr       */
+/*   Updated: 2020/08/27 11:38:44 by heouahes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_epur_str(char *str)
+void	*ft_epur_str(char *str)
 {
 	int i = 0;
 	int space = 0;
-		
+
 	while ((str[i] == ' ') || (str[i] == '\t'))
-		i += 1;
-		while (str[i])
+		i++;
+	while (str[i])
+	{
+		if((str[i] == ' ') || (str[i] == '\t'))
+			space = 1;
+		if(str[i] != ' ' && str[i] != '\t')
 		{
-			space = 0;
-			if((str[i] == ' ') || (str[i] == '\t'))
-				space = 1;
 			if(space == 1)
-				write(1, " ", 1);
-			while (str[i] >= 32 && str[i] <= 126)
 			{
-				write(1, &str[i], 1);
-				i = i + 1;
+				write(1, " ", 1);
+				space = 0;
 			}
-		i++; 
+			write(1, &str[i], 1);
 		}
+		i++; 
+	}
+	write(1, "\n", 1);
+	return (0);
 }
 int main (int ac, char **av)
 {
 	if (ac == 2)
 	{
-	ft_epur_str(av[1]);
+		ft_epur_str(av[1]);
 	}
-	write (1, "\n", 1);
-}
+	return (0);
+}	
